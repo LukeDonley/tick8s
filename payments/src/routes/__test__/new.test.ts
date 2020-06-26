8c21268b19e919a3fe4e235882da94b14d5d6acc
luke donley <titustwimbly@lukes-MacBook-Pro.local>
2020-06-25 20:38:27 -0500

    finish payments service, add listeners

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,15 +1,17 @@
-import request from 'supertest';
-import { app } from '../../app';
 import mongoose from 'mongoose';
-import { Order } from '../../models/order';
+import request from 'supertest';
 import { OrderStatus } from '@hmbmk/ticket-common';
+import { app } from '../../app';
+import { Order } from '../../models/order';
+import { stripe } from '../../stripe';
+import { Payment } from '../../models/payment';
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
-    .post('api/payments')
+    .post('/api/payments')
     .set('Cookie', global.signin())
     .send({
-      token: 'wrehthtg',
+      token: 'asldkfj',
       orderId: mongoose.Types.ObjectId().toHexString()
     })
     .expect(404);
@@ -37,7 +39,6 @@ it('returns a 401 when purchasing an order that doesnt belong to the user', asyn
 
 it('returns a 400 when purchasing a cancelled order', async () => {
   const userId = mongoose.Types.ObjectId().toHexString();
-
   const order = Order.build({
     id: mongoose.Types.ObjectId().toHexString(),
     userId,
@@ -52,7 +53,43 @@ it('returns a 400 when purchasing a cancelled order', async () => {
     .set('Cookie', global.signin(userId))
     .send({
       orderId: order.id,
-      token: 'wragheta'
+      token: 'asdlkfj'
     })
     .expect(400);
+});
+
+it('returns a 201 with valid inputs', async () => {
+  const userId = mongoose.Types.ObjectId().toHexString();
+  const price = Math.floor(Math.random() * 100000);
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price,
+    status: OrderStatus.Created
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id
+    })
+    .expect(201);
+
+  const stripeCharges = await stripe.charges.list({ limit: 50 });
+  const stripeCharge = stripeCharges.data.find((charge) => {
+    return charge.amount === price * 100;
+  });
+
+  expect(stripeCharge).toBeDefined();
+  expect(stripeCharge!.currency).toEqual('usd');
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id
+  });
+  expect(payment).not.toBeNull();
 });
